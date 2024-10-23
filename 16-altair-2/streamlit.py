5e4b07840cdca3f39c47ee8f88a84d1a06b7f441
ZachCutler04 <zach.t.cutler@gmail.com>
2024-10-23 12:50:41 -0600

    push completed streamlit

--- 16-altair-2/streamlit.py
+++ 16-altair-2/streamlit.py
@@ -1,35 +1,35 @@
 import streamlit as st
 
-# import altair as alt
-# import pandas as pd
-# from vega_datasets import data
+import altair as alt
+import pandas as pd
+from vega_datasets import data
 
-# iris_df = data.iris()
-# cars_df = data.cars()
+iris_df = data.iris()
+cars_df = data.cars()
 
 st.write("Hello, World!")
 
-# df = data.cars()
+df = data.cars()
 
-# brush_selection = alt.selection_interval()
+brush_selection = alt.selection_interval()
 
-# base_plot = alt.Chart(df)
+base_plot = alt.Chart(df)
 
-# scatterplot = base_plot.mark_point().encode(
-#     x="Miles_per_Gallon:Q",
-#     y="Weight_in_lbs:Q",
-#     color=alt.condition(brush_selection, "Origin:N", alt.value("gray")),
-#     opacity=alt.condition(brush_selection, alt.value(0.7), alt.value(0.3))
-# ).add_params(
-#     brush_selection
-# )
+scatterplot = base_plot.mark_point().encode(
+    x="Miles_per_Gallon:Q",
+    y="Weight_in_lbs:Q",
+    color=alt.condition(brush_selection, "Origin:N", alt.value("gray")),
+    opacity=alt.condition(brush_selection, alt.value(0.7), alt.value(0.3))
+).add_params(
+    brush_selection
+)
 
-# histogram = base_plot.mark_bar().encode(
-#     y="Origin:N",
-#     color="Origin:N",
-#     x="count():Q",
-# ).transform_filter(
-#     brush_selection
-# )
+histogram = base_plot.mark_bar().encode(
+    y="Origin:N",
+    color="Origin:N",
+    x="count():Q",
+).transform_filter(
+    brush_selection
+)
 
-# st.write(scatterplot & histogram)
+st.write(scatterplot & histogram)
